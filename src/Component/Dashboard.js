0b13b6fa0cacf818cc6aba0110859891584bfdca
sonu <sonu855336@gmail.com>
2023-06-17 20:56:02 +0530

    src

--- src/Component/Dashboard.js
+++ src/Component/Dashboard.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import classes from '../css/Dashboard.module.css'
 import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+
+import pluscard  from '../assests/icons/pluscard.png' 
 function Dashboard() {
   return (
     <div>
@@ -11,9 +13,30 @@ function Dashboard() {
             <FiberManualRecordIcon />
             <section>To Do</section>
            <span><p>4</p></span> 
+           <small>...</small>
+           <img src={pluscard} alt="plus"/>
           </span>
+
+
           <div className={classes.line1}></div>
    </div>
+
+<div className={classes.card}>
+    <div className={classes.cardheader}>
+      <section>Low</section>
+      <span>...</span>  
+    </div>
+    
+    <h6>Brainstorming</h6>
+    <p>Brainstorming brings team members' diverse experience into play</p>
+
+    <div>
+        10 comments
+    </div>
+</div>
+
+
+
  </div> {""}
  <div className={classes.onProgress}>
     
